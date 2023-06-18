7a5c387126da21123b3314f6c28238eeaa1fe848
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-06-18 01:19:39 -0700

    minor: add success echo to build-pkg script

--- mah-pattern-evaluator/build-pkg.sh
+++ mah-pattern-evaluator/build-pkg.sh
@@ -25,3 +25,5 @@ cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen.exe --target web --out-dir "$pkg_dir" ../target/wasm32-unknown-unknown/release/pattern_evaluator.wasm
 
 sed -i '1s/^/\/\/\@ts-nocheck\n\/\* eslint-disable \*\/\n/' "$pkg_dir/pattern_evaluator.js"
+
+echo "Successfully built package in $pkg_dir"
