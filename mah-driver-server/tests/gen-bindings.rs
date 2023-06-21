669db6726ef3a532e6e6c280c35b9920e4940935
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-06-21 12:50:15 -0700

    generate initial bindings

--- mah-driver-server/tests/gen-bindings.rs
+++ mah-driver-server/tests/gen-bindings.rs
@@ -7,15 +7,18 @@ fn bindings_csharp() -> Result<(), Error> {
 	use interoptopus_backend_csharp::overloads::{DotNet, Unity};
 
 	let config = Config {
-		dll_name: "example_library".to_string(),
-		namespace_mappings: NamespaceMappings::new("My.Company"),
+		dll_name: "adaptics_engine".to_string(),
+		namespace_mappings: NamespaceMappings::new("com.github.AdaptiveHaptics.AdapticsEngine"),
 		..Config::default()
 	};
+
+	std::fs::remove_dir_all("bindings").ok();
+	std::fs::create_dir_all("bindings/csharp").ok();
 
 	Generator::new(config, adaptics_engine::my_inventory())
 		.add_overload_writer(DotNet::new())
 		// .add_overload_writer(Unity::new()) //requires use_unsafe or something see https://docs.rs/interoptopus_backend_csharp/latest/interoptopus_backend_csharp/overloads/index.html
-		.write_file("bindings/csharp/Interop.cs")?;
+		.write_file("bindings/csharp/AdapticsEngine.cs")?;
 
 	Ok(())
 }
