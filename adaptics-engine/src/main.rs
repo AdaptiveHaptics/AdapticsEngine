3f94b569f435348371de738b664afb6b40237e12
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-08 20:56:54 -0700

    update cli args

--- adaptics-engine/src/main.rs
+++ adaptics-engine/src/main.rs
@@ -1,25 +1,33 @@
 use clap::Parser;
 // ## !NOTE: do NOT use mod. try to use everything through the lib crate
 
-/// Renders patterns from the web based mid air haptics designer tool, over a WebSocket connection
+/// Adaptics Engine CLI (https://github.com/AdaptiveHaptics/AdapticsEngine)
+///
+/// Allows realtime playback of haptic patterns being developed in the designer (https://adaptivehaptics.github.io/AdapticsDesigner/), using a WebSocket connection
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct MAHServerArgs {
+#[command(author, version, long_about, verbatim_doc_comment)]
+struct AdapticsEngineCliArgs {
     #[clap(short, long, default_value = "127.0.0.1:8037")]
     websocket_bind_addr: String,
 
+    /// Uses a mock haptic device instead of a Ultraleap haptic device.
+    /// This still requires the Ultraleap SDK (and DLLs to be available), but does not attempt to connect to a device.
+    /// Mock device configuration is DEVICE_UPDATE_RATE=20000hz, CALLBACK_RATE=500hz
     #[clap(short='m', long)]
     use_mock_streaming: bool,
 
-    #[clap(short, long)]
+    /// Disables hosting the WebSocket server. Likely only useful for testing.
+    #[clap(long)]
     no_network: bool,
 
-    #[clap(short, long)]
+    /// Disables attempts to connect to the Ultraleap tracking service (leap motion controller).
+    /// Enable this if you want to run the engine on a machine without the Gemini SDK installed.
+    #[clap(short='t', long)]
     no_tracking: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let cli_args = MAHServerArgs::parse();
+    let cli_args = AdapticsEngineCliArgs::parse();
 
     adaptics_engine::run_threads_and_wait(
         cli_args.use_mock_streaming,
