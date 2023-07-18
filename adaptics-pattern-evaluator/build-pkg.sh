ac95a9e652ebe1187d1681bb1dc301cf3f122a7e
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-18 13:05:41 -0700

    enable --weak-refs --reference-types (although both are unused in gui)

--- adaptics-pattern-evaluator/build-pkg.sh
+++ adaptics-pattern-evaluator/build-pkg.sh
@@ -25,7 +25,7 @@ node -p "import('json-schema-to-typescript').then(j => j.compileFromFile('$json_
 # mv bindings "$pkg_dir"
 
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen.exe --target web --out-dir "$pkg_dir" ../target/wasm32-unknown-unknown/release/pattern_evaluator.wasm
+wasm-bindgen.exe --target web --weak-refs --reference-types --out-dir "$pkg_dir" ../target/wasm32-unknown-unknown/release/pattern_evaluator.wasm
 
 sed -i '1s/^/\/\/\@ts-nocheck\n\/\* eslint-disable \*\/\n/' "$pkg_dir/pattern_evaluator.js"
 
