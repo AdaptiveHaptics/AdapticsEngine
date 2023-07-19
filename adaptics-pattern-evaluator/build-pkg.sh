ee2f866988aac4b53bab28b7d5f75113219a2ac4
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-19 13:31:37 -0700

    add CI/CD for adaptics-pattern-evaluator (wasm) on npm

--- adaptics-pattern-evaluator/build-pkg.sh
+++ adaptics-pattern-evaluator/build-pkg.sh
@@ -17,15 +17,18 @@ cargo clippy
 cargo test
 
 # schemars
-cargo run -- "$json_schema_file"
+cargo run -- "$json_schema_file" >/dev/null
 node -p "import('json-schema-to-typescript').then(j => j.compileFromFile('$json_schema_file', { additionalProperties: false }).then(ts => fs.writeFileSync('$typescript_defs_file', ts)))"
 
-# ts_rs
-# cargo test # the ts_rs schema is generated inside of tests
-# mv bindings "$pkg_dir"
+#cargo build --target wasm32-unknown-unknown --release
+#wasm-bindgen.exe --target web --weak-refs --reference-types --out-dir "$pkg_dir" ../target/wasm32-unknown-unknown/release/pattern_evaluator.wasm
+wasm-pack.exe build --target web --weak-refs --reference-types --out-dir "$pkg_dir" --release
 
-cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen.exe --target web --weak-refs --reference-types --out-dir "$pkg_dir" ../target/wasm32-unknown-unknown/release/pattern_evaluator.wasm
+# include all pkg files in npm package
+rm "$pkg_dir/.gitignore"
+node -e "const fs=require('fs'); const pkg=JSON.parse(fs.readFileSync('$pkg_dir/package.json')); delete pkg.files; fs.writeFileSync('$pkg_dir/package.json', JSON.stringify(pkg, null, 2))"
+
+wasm-pack.exe pack "$pkg_dir" #--scope adaptics --no-typescript
 
 sed -i '1s/^/\/\/\@ts-nocheck\n\/\* eslint-disable \*\/\n/' "$pkg_dir/pattern_evaluator.js"
 
