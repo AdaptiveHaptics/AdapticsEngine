fb91befe1871da3b4bef6f10b0a8760812b30cc6
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2023-07-30 18:51:11 -0700

    build d.ts for AdapticsWSServerMessage schema

--- adaptics-engine/build-pkg.sh
+++ adaptics-engine/build-pkg.sh
@@ -14,6 +14,10 @@ cargo clippy
 cargo test --release # also generates bindings (see test/gen-bindings.rs)
 cargo build --release
 
+json_schema_file="bindings/websocket/AdapticsWSServerMessage.json"
+typescript_defs_file="${json_schema_file%.json}.d.ts"
+node -p "import('json-schema-to-typescript').then(j => j.compileFromFile('$json_schema_file', { additionalProperties: false }).then(ts => fs.writeFileSync('$typescript_defs_file', ts)))"
+
 cp ../target/release/adaptics-engine-cli.exe "$pkg_dir/adaptics-engine-cli.exe"
 mkdir "$pkg_dir/unity"
 cp ../target/release/adaptics_engine.dll "$pkg_dir/unity/adaptics_engine.dll"
